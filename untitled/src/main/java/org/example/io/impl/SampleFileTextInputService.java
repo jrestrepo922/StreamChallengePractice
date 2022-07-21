47b60c717394c77b9123b14002d2f83e880cf048
Juan Martin Restrepo <tinto922@gmail.com>
2022-07-21 16:56:59 -0400

    stuff

--- untitled/src/main/java/org/example/io/impl/SampleFileTextInputService.java
+++ untitled/src/main/java/org/example/io/impl/SampleFileTextInputService.java
@@ -1,12 +1,26 @@
 package org.example.io.impl;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.example.io.TextInputService;
 
 public class SampleFileTextInputService implements TextInputService {
-    private static final String FILE_NAME = "book.txt";
+    private static final String FILE_NAME = "C:\\Users\\Work\\Development\\BlackRockXFlatiron\\Week4\\Lab\\StreamChallengePractice\\untitled\\book.txt";
     @Override
     public String getText() {
-        // TODO
+        try{
+            Path path =  Path.of(FILE_NAME);
+            String content = Files.readString(path);
+            return  content;
+
+        } catch (IOException ioException){
+            System.out.println("Something when wrong");
+            
+        }
         return null;
     }
+
+
 }
--- untitled/src/main/java/org/example/statistics/TextStatisticsService.java
+++ untitled/src/main/java/org/example/statistics/TextStatisticsService.java
@@ -1,11 +1,23 @@
 package org.example.statistics;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
 import org.example.statistics.model.TextStatistics;
 
 public class TextStatisticsService {
     public TextStatistics getStatistics(String text) {
         // TODO
         // Please use Streams
+        // getting string from the input and will be splitting on space to get all the words
+        List<String> data = Arrays.asList(text.replaceAll("[^a-zA-Z0-9]", " ").split("\\s+"));
+        // doing some statistics on
+        System.out.println(data);
         return null;
+        
     }
+
 }
